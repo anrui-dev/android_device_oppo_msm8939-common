cfc7ff2c6a976bcc3c62051fcf15492d1acc4ecb
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    msm8939-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8939.mk
+++ msm8939.mk
@@ -19,6 +19,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS += *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
