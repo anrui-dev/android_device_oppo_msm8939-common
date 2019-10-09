103171c1eb2b10920da07a93124614340f214b23
sb6596 <shubhamprince111@gmail.com>
2019-10-09 12:07:00 +0000

    msm8939-common: Switch to cryptfs_hw hidl service
    Change-Id: Ie924f67f90d64609af3738f20848f08c5c26b781

--- msm8939.mk
+++ msm8939.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # For config.fs
 PRODUCT_PACKAGES += \
     fs_config_files
