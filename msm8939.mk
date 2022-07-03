30b0a3f61f7f25a7ad2cc8ca7a7d70e414795ced
anrui2032 <anrui2032@gmail.com>
2022-07-03 04:05:39 +0000

    Revert "msm8939: Enforce RRO on framework-res"
    This reverts commit c8a4cbb680afeb731bce8a49f19c7021d5b2fb48.
    
    Change-Id: Id3eaa852455f74ef6a830b59b8d9f80c3726d0b0

--- msm8939.mk
+++ msm8939.mk
@@ -17,8 +17,6 @@
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
-
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
