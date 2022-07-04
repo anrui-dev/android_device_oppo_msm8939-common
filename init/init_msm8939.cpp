6382d97f6c17f5c7e92e9680369950e3ead1b4da
Michael Bestas <mikeioannina@cyanogenmod.org>
2022-07-04 09:49:52 +0000

    msm8939-common: Allow vendor init to override props
    Change-Id: Iee06664aee1e59596aa12e2da11319f82767193a

--- init/init_msm8939.cpp
+++ init/init_msm8939.cpp
@@ -34,6 +34,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
@@ -45,6 +48,17 @@ using android::base::GetProperty;
 using android::base::ReadFileToString;
 using android::base::Trim;
 using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 static void init_alarm_boot_properties()
 {
